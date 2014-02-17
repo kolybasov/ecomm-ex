bc6e7faca145367b1d91a29c215c396e7aeb9bfe
kolybasov <kolybasov@yandex.ru>
2014-02-17 19:52:50 +0200

    Added default value for status

---
+++ app/database/migrations/2014_02_17_170019_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function($table) {
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->tinyinteger('payment');
+			$table->integer('status')->unsigned()->default(1);
+			$table->string('comment');
+			$table->decimal('total', 6, 2);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
